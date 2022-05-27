64a5e39f796a55bc1960915d3033dc680ca88dde
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2022-05-27 23:06:45 +0100

    first commit for finding anagrams

---
+++ main.py
@@ -0,0 +1,20 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    word = input("word:")
+    anagram = input("anagram:")
+    sorted_word = sorted(word)
+    sorted_anagram = sorted(anagram)
+    
+    
+    if sorted_word == sorted_anagram:
+        print ("True")
+        
+    else:
+        print ("False")
+
